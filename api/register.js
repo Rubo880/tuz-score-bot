18046f3feaec45d9ef44b7c8c04c179ece27f4ff
Рубен <258628@edu.fa.ru>
2026-09-20 11:31:55 +0300

    Add Tuz roll to Telegram command menu

--- api/register.js
+++ api/register.js
@@ -32,6 +32,7 @@ export default async function handler(req, res) {
         { command: "me", description: "Показать мой счёт" },
         { command: "rules", description: "Правила и античит" },
         { command: "undo", description: "Владелец: отменить очки за сообщение" },
+        { command: "tuzroll", description: "Козырной или опущенный туз раз в 24ч" },
         { command: "web", description: "Открыть веб-лидерборд" }
       ]
     });
